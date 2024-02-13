2b8832cce36f68dd989e3c756a757c5dba28bf11
Abhishek Sable <abhisheksable@Abhisheks-MacBook-Air.local>
2024-02-13 16:29:34 +0530

    Downgrading project to use ribbon load balancer as ribbon was deprecated. Added Spring boot starter ribbon depedency

--- src/main/java/com/empmgmt/employeeservice/feignclients/AddressClient.java
+++ src/main/java/com/empmgmt/employeeservice/feignclients/AddressClient.java
@@ -1,12 +1,14 @@
 package com.empmgmt.employeeservice.feignclients;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.empmgmt.employeeservice.dto.AddressDTO;
 
-@FeignClient(name="address-service",url="http://localhost:8080/address-app/api")
+@FeignClient(name="address-service",path="/address-app/api")
+@RibbonClient(name= "address-service")
 public interface AddressClient {
 
 	@GetMapping("/address/{id}")
